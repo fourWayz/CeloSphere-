7d81fc39957b6b2e522eef7064e928a085331f0f
fourWayz <157867069+fourWayz@users.noreply.github.com>
2025-03-23 12:43:27 +0000

    comment and post like  tests passed

--- packages/hardhat/test/Celosphere.ts
+++ packages/hardhat/test/Celosphere.ts
@@ -23,7 +23,7 @@ describe('Celosphere', function () {
   it('should register a user', async function () {
     const username = 'user1';
 
-    await (socialMedia.connect(user1)).registerUser(username);
+    await (socialMedia.connect(user1)as Celosphere).registerUser(username);
     const user = await socialMedia.users(user1.address);
 
     expect(user.username).to.equal(username);
@@ -34,7 +34,7 @@ describe('Celosphere', function () {
 
   it('should create a post', async function () {
     const content = 'This is a test post';
-    await (socialMedia.connect(user1)).createPost(content);
+    await (socialMedia.connect(user1)as Celosphere).createPost(content);
     const postsCount = await socialMedia.getPostsCount();
     expect(postsCount.toString()).to.equal('1');
 
@@ -46,5 +46,27 @@ describe('Celosphere', function () {
 
   });
  
+  it('should like a post', async function () {
+    await (socialMedia.connect(user2) as Celosphere).registerUser('user2');
+    await (socialMedia.connect(user2) as Celosphere).likePost(0);
+    const post = await socialMedia.getPost(0);
+    expect(post.likes.toString()).to.equal('1');
+    expect(post.author).to.equal(user1.address);
+  
+  });
+
+  it('should add a comment to a post', async function () {
+    const content = 'This is a test post';
+    const commentContent = 'This is a test comment';
+
+    await (socialMedia.connect(user2) as Celosphere).addComment(0, commentContent);
+
+    const comment = await socialMedia.getComment(0, 0);
+    expect(comment.commenter).to.equal(user2.address);
+    expect(comment.content).to.equal(commentContent);
+
+    const post = await socialMedia.getPost(0);
+    expect(post.commentsCount.toString()).to.equal('1');
+  });
   
 });
